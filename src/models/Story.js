c93678df1075f3ea9684fdc0452391f0aea31dd7
Quan Than <quantv654@gnail.com>
2025-09-15 08:57:58 +0700

    refactor: refactor project to MVC-S

--- src/models/Story.js
+++ src/models/Story.js
@@ -1,9 +1,29 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose from 'mongoose'
 
-const StorySchema = new Schema({
-	title: String,
-	url: String,
-	author: String,
-})
+const storySchema = new mongoose.Schema(
+	{
+		title: { type: String, required: true },
+		storyUrl: { type: String, required: true, unique: true }, // URL gốc của truyện
+		author: { type: String },
+		artist: { type: String },
+		numberOfVolumes: { type: Number },
+		coverImageUrl: { type: String }, // URL ảnh bìa gốc
+		summary: { type: String },
+		lastScraped: { type: Date, default: Date.now }, // Lần cuối cào dữ liệu
+		volumes: [
+			{
+				title: { type: String },
+				chapters: [
+					{
+						title: { type: String },
+						url: { type: String },
+					},
+				],
+			},
+		],
+		favorite: { type: Boolean, default: false },
+	},
+	{ timestamps: true },
+)
 
-export default mongoose.model('Story', StorySchema)
+export default mongoose.model('Story', storySchema)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const volume = await scrapeChapterList(
+		'https://docln.sbs/ai-dich/23025-ban-hang-rong-o-di-gioi-toi-co-the-ve-nha-bat-cu-khi-nao-minh-muon',
+	)
+	console.log(JSON.stringify(volume))
